f0ec78085275aafec6bada530dc9742faf81d1fa
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2025-05-02 11:50:36 +0100

    Amended new endpoint to include query string

--- lambdas/question1.ts
+++ lambdas/question1.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = createDDbDocClient();
 
@@ -8,6 +8,7 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   try {
     const movieId = event.pathParameters?.movieId;
     const role = event.pathParameters?.role;
+    const verbose = event.queryStringParameters?.verbose === "true";
 
     if (!movieId || !role) {
       return {
@@ -17,29 +18,46 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
       };
     }
 
-    const command = new GetCommand({
-      TableName: process.env.TABLE_NAME,
-      Key: {
-        movieId: Number(movieId),
-        role: role,
-      },
-    });
+    if (verbose) {
+      const command = new QueryCommand({
+        TableName: process.env.TABLE_NAME,
+        KeyConditionExpression: "movieId = :movieId",
+        ExpressionAttributeValues: {
+          ":movieId": Number(movieId),
+        },
+      });
 
-    const response = await client.send(command);
-
-    if (!response.Item) {
+      const response = await client.send(command);
       return {
-        statusCode: 404,
+        statusCode: 200,
         headers: { "content-type": "application/json" },
-        body: JSON.stringify({ error: "Crew member not found" }),
+        body: JSON.stringify(response.Items),
+      };
+    } else {
+      const command = new GetCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: {
+          movieId: Number(movieId),
+          role: role,
+        },
+      });
+
+      const response = await client.send(command);
+
+      if (!response.Item) {
+        return {
+          statusCode: 404,
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify({ error: "Crew member not found" }),
+        };
+      }
+
+      return {
+        statusCode: 200,
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(response.Item),
       };
     }
-
-    return {
-      statusCode: 200,
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(response.Item),
-    };
   } catch (error: any) {
     console.log(JSON.stringify(error));
     return {
